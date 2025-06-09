b7a8891fc8e8818e732686c0b5aaf70ac0164e3a
unknown <272565@student.pwr.edu.pl>
2025-06-09 10:11:35 +0200

    Pierwszy push

---
+++ utils.py
@@ -0,0 +1,13 @@
+# przykladowe funckje utils.py
+
+def add(a: int, b: int) -> int:
+	return a + b
+
+def substract(a: int, b: int) -> int:
+	return a - b
+
+def multiply(a: int, b: int) -> int:
+	return a * b
+
+def divide(a: int, b: int) -> float:
+	return a / b
